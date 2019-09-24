e8fbba8ca04c45f02d8a1eadb5d9292af471ab0a
George Apazidis <geoapa@gmail.com>
2019-09-24 14:26:13 -0400

    created Todoitem component

---
+++ src/components/Todoitem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class TodoItem extends Component {
+  state = {  }
+  render() { 
+    return (  
+      <div>
+        <p>{ this.props.todo.title }</p>
+      </div>
+    );
+  }
+}
+ 
+export default TodoItem;
